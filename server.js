436dc0951bac6d7c6e2fc5840122b901d3a9677d
Rozali <yuldashevrozali08@gmail.com>
2025-08-30 23:41:16 +0500

    asdsa

--- server.js
+++ server.js
@@ -11,12 +11,14 @@ const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/myapp";
 
 // Middleware
-app.use(cors());
 app.use(express.json());
 
 app.use(cors({
-  origin: "http://localhost:3000"
+  origin: ["http://localhost:3000", "https://unco-frontend.onrender.com"], // frontend URL lar
+  methods: ["GET", "POST", "PUT", "DELETE"], // ruxsat etilgan metodlar
+  credentials: true
 }));
+
 
 // MongoDB ulanish
 mongoose.set("strictQuery", true);
